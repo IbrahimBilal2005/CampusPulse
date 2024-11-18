97e79f72a8f574bcb894cb7153b123ffd0ba5708
Tanay Langhe <tanaylanghe@gmail.com>
2024-11-18 04:43:29 -0500

    Added code to match design choices decided for this screen

--- src/main/Java/views/Login_screen.java
+++ src/main/Java/views/Login_screen.java
@@ -17,6 +17,17 @@ public class Login_screen {
         frame.setSize(screenSize.width/2, screenSize.height/2);
         frame.setLocationRelativeTo(null);
 
+        //Made a banner with application name at the top
+        JLabel bannerLabel = new JLabel("CampusPulse");
+        bannerLabel.setFont(new Font("Arial", Font.BOLD, 24)); // Set font size and style
+        bannerLabel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0)); // Add padding around the text
+        bannerLabel.setAlignmentX(Component.CENTER_ALIGNMENT); // Center the label in BoxLayout
+
+        //Add a line to show that the above is a banner of sorts
+        JSeparator separator = new JSeparator();
+        separator.setMaximumSize(new Dimension(Integer.MAX_VALUE, 1)); // Make the separator span the full width
+        separator.setForeground(Color.BLACK);
+
         //Create panel (container) for the username field
         JPanel username_panel = new JPanel();
         username_panel.add(new JLabel("Username:"));
@@ -27,12 +38,13 @@ public class Login_screen {
         password_panel.add(new JTextField(20));
         //Added in login button
         JPanel button_panel = new JPanel();
-        button_panel.setLayout(new BoxLayout(button_panel, BoxLayout.X_AXIS));
         button_panel.add(new JButton("Login"));
 
-        //Create the main panel and set the frame to display it 
+        //Create the main panel and set the frame to display it the order matters since some things should not be centered within the frame
         JPanel main_panel = new JPanel();
         main_panel.setLayout(new BoxLayout(main_panel, BoxLayout.Y_AXIS));
+        main_panel.add(bannerLabel);
+        main_panel.add(separator);
         main_panel.add(Box.createVerticalGlue());
         main_panel.add(username_panel);
         main_panel.add(password_panel);
