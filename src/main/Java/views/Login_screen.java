db817127f167371e8c8aa1b6e4a8966c1a38bbe5
Tanay Langhe <tanaylanghe@gmail.com>
2024-11-17 22:16:49 -0500

    Create frame for login screen

--- src/main/Java/views/Login_screen.java
+++ src/main/Java/views/Login_screen.java
@@ -1,4 +1,23 @@
 package views;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Login_screen {
+
+    public static void main(String[] args) {
+        // Create a new frame
+        JFrame frame = new JFrame("Login Screen");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(screenSize.width/2, screenSize.height/2);
+        frame.setLocationRelativeTo(null);
+
+
+        
+    }
 }
+
