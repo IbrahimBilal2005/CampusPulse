f29ffd4bea213adaa6d96885787acb73ed820194
AhmadIbrahimWaqas <157913756+AhmadIbrahimWaqas@users.noreply.github.com>
2024-11-21 16:47:11 -0500

    Started change password use case

--- src/main/Java/use_case/change_password/ChangePasswordInputBoundary.java
+++ src/main/Java/use_case/change_password/ChangePasswordInputBoundary.java
@@ -1,4 +1,9 @@
 package use_case.change_password;
 
 public interface ChangePasswordInputBoundary {
+    /**
+     * Execute the Change Password Use Case.
+     * @param changePasswordInputData the input data for this use case
+     */
+    void execute(ChangePasswordInputData changePasswordInputData);
 }
--- src/main/Java/use_case/change_password/ChangePasswordInputData.java
+++ src/main/Java/use_case/change_password/ChangePasswordInputData.java
@@ -1,4 +1,14 @@
 package use_case.change_password;
 
 public class ChangePasswordInputData {
+    private final String username;
+    private final String currentPassword;
+    private final String newPassword;
+
+    public ChangePasswordInputData(String username, String currentPassword, String newPassword) {
+        this.username = username;
+        this.currentPassword = currentPassword;
+        this.newPassword = newPassword;
+    }
+
 }
