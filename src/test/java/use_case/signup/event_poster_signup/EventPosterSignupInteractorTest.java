18cd0208ca414fd3dc546f9b38bdc7d7c1d470c3
IbrahimBilal2005 <ibrahim.bilal@mail.utoronto.ca>
2024-11-24 11:49:29 -0500

    completed success test in EventPosterSignupInteractorTest.java

--- src/test/java/use_case/signup/event_poster_signup/EventPosterSignupInteractorTest.java
+++ src/test/java/use_case/signup/event_poster_signup/EventPosterSignupInteractorTest.java
@@ -1,16 +1,45 @@
 package use_case.signup.event_poster_signup;
 
+import data_access.InMemoryUserDataAccessObject;
+import entity.EventPosterCreationStrategy;
 import org.junit.jupiter.api.Test;
 import entity.Account;
-import entity.UserCreationStrategy;
 import entity.AccountCreationStrategy;
-//import data_access.DATA_ACCESS_FILE
+import use_case.signup.UserSignupDataAccessInterface;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class EventPosterSignupInteractorTest {
 
     @Test
     void successTest() {
+        EventPosterSignupInputData inputData = new EventPosterSignupInputData("username", "password", "password", "Organization Name", "sopLink");
+        UserSignupDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
+        EventPosterSignupOutputBoundary successPresenter = new EventPosterSignupOutputBoundary() {
 
+            @Override
+            public void prepareSuccessView(EventPosterSignupOutputData eventPoster) {
+                assertEquals("username", eventPoster.getUsername());
+                assertTrue(userRepository.existsByName("username"));
+            }
+
+            @Override
+            public void prepareFailView(String errorMessage) {
+                fail("User case failure is unexpected.");
+            }
+
+            @Override
+            public void switchToLoginView() {
+                //expected
+            }
+
+            @Override
+            public void switchToBaseView() {
+                //expected
+            }
+        };
+        EventPosterSignupInteractor interactor = new EventPosterSignupInteractor(userRepository, successPresenter, new EventPosterCreationStrategy());
+        interactor.execute(inputData);
     }
 
     @Test
