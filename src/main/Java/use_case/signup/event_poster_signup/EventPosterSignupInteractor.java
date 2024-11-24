56c41a2a9bd115900d0b57666e29d9a6e8556914
IbrahimBilal2005 <ibrahim.bilal@mail.utoronto.ca>
2024-11-24 11:03:47 -0500

    Completed execute method in EventPosterSignupInteractor.java

--- src/main/Java/use_case/signup/event_poster_signup/EventPosterSignupInteractor.java
+++ src/main/Java/use_case/signup/event_poster_signup/EventPosterSignupInteractor.java
@@ -1,31 +1,47 @@
 package use_case.signup.event_poster_signup;
 
-// import entity.Account;
-// import entity.UserFactory
+import entity.Account;
+import entity.AccountCreationStrategy;
 
 public class EventPosterSignupInteractor implements EventPosterSignupInputBoundary{
 
-    private final EventPosterUserDataAccessInterface userDataAccessInterface;
+    private final EventPosterUserDataAccessInterface userDataAccessObject;
     private final EventPosterSignupOutputBoundary userPresenter;
-    // private final Userfactory userFactory;
+    private final AccountCreationStrategy accountCreator;
 
-    public EventPosterSignupInteractor(EventPosterUserDataAccessInterface userDataAccessInterface,
-                                       EventPosterSignupOutputBoundary eventPosterSignupOutputBoundary) {
-        this.userDataAccessInterface = userDataAccessInterface;
+    public EventPosterSignupInteractor(EventPosterUserDataAccessInterface userDataAccessObject,
+                                       EventPosterSignupOutputBoundary eventPosterSignupOutputBoundary,
+                                       AccountCreationStrategy accountCreator) {
+        this.userDataAccessObject = userDataAccessObject;
         this.userPresenter = eventPosterSignupOutputBoundary;
+        this.accountCreator = accountCreator;
     }
 
     @Override
     public void execute(EventPosterSignupInputData eventPosterSignupInputData){
-        // Logic for creating the event poster user. Dependent on database and user factories.
-        // will work on implementing this next.
+        if (userDataAccessObject.existsByName(eventPosterSignupInputData.getUsername())) {
+            userPresenter.prepareFailView("User already exists.");
+        }
+        else if (!eventPosterSignupInputData.getPassword().equals(eventPosterSignupInputData.getRepeatPassword())) {
+            userPresenter.prepareFailView("Passwords do not match.");
+        }
+        else {
+            final Account eventPoster = accountCreator.createAccount(
+                    eventPosterSignupInputData.getUsername(),
+                    eventPosterSignupInputData.getPassword(),
+                    eventPosterSignupInputData.getRepeatPassword(),
+                    eventPosterSignupInputData.getOrganizationName(),
+                    eventPosterSignupInputData.getSopLink());
+
+            userDataAccessObject.save(eventPoster);
+            final EventPosterSignupOutputData signupOutputData = new EventPosterSignupOutputData(eventPoster.getUsername(), false);
+            userPresenter.prepareSuccessView(signupOutputData);
+        }
     }
 
     @Override
     public void switchToLoginView() { userPresenter.switchToLoginView(); }
 
     @Override
-    public void switchToBaseView() {
-        userPresenter.switchToBaseView();
-    }
+    public void switchToBaseView() { userPresenter.switchToBaseView(); }
 }
