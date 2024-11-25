7620a97b878bb1d4299e8615688e6deb373553d9
Towaiji <aliyasiraltowaiji@gmail.com>
2024-11-24 22:53:29 -0500

    added authentication service which helps with adding accounts to the firebase

---
+++ src/main/Java/App/auth/AuthService.java
@@ -0,0 +1,20 @@
+package App.auth;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserRecord;
+
+public class AuthService {
+    public static void createUser(String email, String password) {
+        UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                .setEmail(email)
+                .setPassword(password);
+
+        try {
+            UserRecord userRecord = FirebaseAuth.getInstance().createUser(request);
+            System.out.println("Successfully created new user: " + userRecord.getUid());
+        } catch (Exception e) {
+            System.err.println("Error creating user: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
