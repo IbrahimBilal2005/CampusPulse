ae99221118e9148bb29315906b16d157083346e3
Towaiji <aliyasiraltowaiji@gmail.com>
2024-11-25 16:45:31 -0500

    added role to UserCreationStrategy.java

--- src/main/Java/entity/UserCreationStrategy.java
+++ src/main/Java/entity/UserCreationStrategy.java
@@ -7,13 +7,14 @@ public class UserCreationStrategy implements AccountCreationStrategy {
 
     @Override
     public Account createAccount(String username, String password, Object... params) {
-        // Assuming params are [firstName, lastName, age, gender, interests]
+        // Assuming params are [firstName, lastName, age, gender, interests, role]
         String firstName = (String) params[0];
         String lastName = (String) params[1];
         Integer age = (Integer) params[2];
         String gender = (String) params[3];
         List<String> interests = (List<String>) params[4];
+        String role = (String) params[5];
 
-        return new User(username, password, firstName, lastName, age, gender, interests);
+        return new User(username, password, firstName, lastName, age, gender, interests, role);
     }
 }
