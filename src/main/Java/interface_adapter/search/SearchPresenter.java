3c5d17eb9b37f00b67b8ea34b9a9bcf07ffb6585
Waleed <wsakrabi@gmail.com>
2024-11-26 13:44:49 -0500

    fixed the use of presenter in the interactor

--- src/main/Java/interface_adapter/search/SearchPresenter.java
+++ src/main/Java/interface_adapter/search/SearchPresenter.java
@@ -2,7 +2,6 @@ package interface_adapter.search;
 
 import use_case.search.SearchOutputBoundary;
 import use_case.search.SearchOutputData;
-
 import java.util.Collections;
 
 public class SearchPresenter implements SearchOutputBoundary {
@@ -20,15 +19,18 @@ public class SearchPresenter implements SearchOutputBoundary {
     public void setPassView(SearchOutputData outputData) {
         this.outputData = outputData;
         this.errorMessage = null;  // Reset the error message when passing data
+        // Update the viewModel with the events
         viewModel.setSearchResults(outputData.getEvents() != null ? outputData.getEvents() : Collections.emptyList());
-        viewModel.setError(null);
+        viewModel.setError(null);  // Clear the error message in the viewModel
     }
 
     @Override
     public void setFailView(String errorMessage) {
         this.outputData = null;  // Reset the output data when failing
         this.errorMessage = errorMessage;
+        // Set the empty list for search results
         viewModel.setSearchResults(Collections.emptyList());
+        // Set the error message in the viewModel
         viewModel.setError(errorMessage);
     }
 }
--- src/main/Java/use_case/search/SearchInteractor.java
+++ src/main/Java/use_case/search/SearchInteractor.java
@@ -1,13 +1,16 @@
 package use_case.search;
 
 import entity.Event;
+import use_case.search.SearchOutputBoundary;
 import java.util.List;
 
 public class SearchInteractor implements SearchInputBoundary {
     private final SearchDataAccessInterface dataAccess;
+    private final SearchOutputBoundary searchPresenter;
 
-    public SearchInteractor(SearchDataAccessInterface dataAccess) {
+    public SearchInteractor(SearchDataAccessInterface dataAccess, SearchOutputBoundary searchPresenter) {
         this.dataAccess = dataAccess;
+        this.searchPresenter = searchPresenter;
     }
 
     @Override
@@ -19,9 +22,9 @@ public class SearchInteractor implements SearchInputBoundary {
             // If the query is empty, show all events
             List<Event> allEvents = dataAccess.getAllEvents();
             if (allEvents.isEmpty()) {
-                presentError("No events available.");
+                searchPresenter.setFailView("No events available.");
             } else {
-                presentSearchResults(allEvents);
+                searchPresenter.setPassView(new SearchOutputData(allEvents));
             }
             return;
         }
@@ -31,23 +34,9 @@ public class SearchInteractor implements SearchInputBoundary {
 
         // If no events are found, return an error message
         if (events.isEmpty()) {
-            presentError("No events found matching your search query.");
+            searchPresenter.setFailView("No events found matching your search query.");
         } else {
-            presentSearchResults(events);
+            searchPresenter.setPassView(new SearchOutputData(events));
         }
-    }
-
-    private void presentSearchResults(List<Event> events) {
-        // This method is where you'd send the results to the view or presenter
-        // For example, you might pass the events to a presenter or return them to the controller
-        System.out.println("Search Results:");
-        for (Event event : events) {
-            System.out.println(event.getName()); // Or whatever you'd like to do with the events
-        }
-    }
-
-    private void presentError(String message) {
-        // This method will display the error message to the user
-        System.out.println("Error: " + message);
     }
 }
