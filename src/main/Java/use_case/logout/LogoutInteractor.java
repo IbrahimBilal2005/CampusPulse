813713e1dacb0dc3c512dcf0e344fcfc7e716fb2
IbrahimBilal2005 <ibrahim.bilal@mail.utoronto.ca>
2024-11-27 12:42:06 -0500

    completed LogoutInteractor

--- src/main/Java/use_case/logout/LogoutInteractor.java
+++ src/main/Java/use_case/logout/LogoutInteractor.java
@@ -1,4 +1,23 @@
 package use_case.logout;
 
-public class LogoutInteractor {
+/**
+ * Logout Interactor.
+ */
+public class LogoutInteractor implements LogoutInputBoundary {
+    private LogoutUserDataAccessInterface userDataAccessObject;
+    private LogoutOutputBoundary logoutPresenter;
+
+    public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessObject,
+                            LogoutOutputBoundary logoutPresenter) {
+        this.userDataAccessObject = userDataAccessObject;
+        this.logoutPresenter = logoutPresenter;
+    }
+
+    @Override
+    public void execute(LogoutInputData logoutInputData) {
+        final String username = logoutInputData.getUsername();
+        userDataAccessObject.setCurentusername(null);
+        final LogoutOutputData logoutOutputData = new LogoutOutputData(username, false);
+        logoutPresenter.prepareSuccessView(logoutOutputData);
+    }
 }
