d7ac6ad7e81a63964da006633da64ea752129226
IbrahimBilal2005 <ibrahim.bilal@mail.utoronto.ca>
2024-11-26 23:22:08 -0500

    Adjusted MongoConnection file to implement readDB interface

--- src/main/Java/data_access/MongoDB/MongoConnection.java
+++ src/main/Java/data_access/MongoDB/MongoConnection.java
@@ -2,23 +2,38 @@ package data_access.MongoDB;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
-public class MongoConnection {
 
-    private static MongoClient mongoClient;
-    private static final String CONNECTION_STRING = "mongodb+srv://ibrahimbilal:Ibrahim123@cluster0.ngph3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+public class MongoConnection implements readDBInterface{
 
-    public static MongoDatabase getDatabase(String dbName) {
-        if (mongoClient == null) {
-            mongoClient = MongoClients.create(CONNECTION_STRING);
-        }
-        return mongoClient.getDatabase(dbName);
+    private MongoClient mongoClient = MongoClients.create("mongodb+srv://ibrahimbilal:Ibrahim123@cluster0.ngph3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    private MongoDatabase database = mongoClient.getDatabase("CampusPulse");
+
+    private MongoCollection<Document> eventsCollection = database.getCollection("events");
+    private MongoCollection<Document> eventPostersCollection = database.getCollection("eventPosters");
+    private MongoCollection<Document> usersCollection = database.getCollection("users");
+    private MongoCollection<Document> adminsCollection = database.getCollection("admins");
+
+    @Override
+    public MongoCollection<Document> getEventsCollection() {
+        return eventsCollection;
     }
 
-    public static void closeConnection() {
-        if (mongoClient != null) {
-            mongoClient.close();
-        }
+    @Override
+    public MongoCollection<Document> getUsersCollection() {
+        return usersCollection;
+    }
+
+    @Override
+    public MongoCollection<Document> getEventPostersCollection() {
+        return eventPostersCollection;
+    }
+
+    @Override
+    public MongoCollection<Document> getAdminCollection() {
+        return adminsCollection;
     }
 }
