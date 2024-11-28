54e8b801c7d6beb0a3ae274c8a9e85b9d1bd5574
Towaiji <aliyasiraltowaiji@gmail.com>
2024-11-28 16:07:09 -0500

    implemented AdminApprovalController

---
+++ src/main/Java/interface_adapter/admin_approval/AdminApprovalController.java
@@ -0,0 +1,17 @@
+package interface_adapter.admin_approval;
+
+import use_case.admin_account_approval.AdminApprovalInputBoundary;
+import use_case.admin_account_approval.AdminApprovalInputData;
+
+public class AdminApprovalController {
+    private final AdminApprovalInputBoundary inputBoundary;
+
+    public AdminApprovalController(AdminApprovalInputBoundary inputBoundary) {
+        this.inputBoundary = inputBoundary;
+    }
+
+    public void approveUser(String targetUid) {
+        AdminApprovalInputData inputData = new AdminApprovalInputData(targetUid);
+        inputBoundary.approveUser(inputData);
+    }
+}
