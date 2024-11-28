2469de2b74dc4322bbc569e5223bb4e546db9210
Towaiji <aliyasiraltowaiji@gmail.com>
2024-11-27 22:41:05 -0500

    implemented AdminApprovalOutputData

--- src/main/Java/use_case/admin_account_approval/AdminApprovalOutputData.java
+++ src/main/Java/use_case/admin_account_approval/AdminApprovalOutputData.java
@@ -1,4 +1,13 @@
 package use_case.admin_account_approval;
 
 public class AdminApprovalOutputData {
+    private final String message;
+
+    public AdminApprovalOutputData(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
