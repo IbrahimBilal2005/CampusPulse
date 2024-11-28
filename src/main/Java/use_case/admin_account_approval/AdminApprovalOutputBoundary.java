019096b1c4af828a7c07bf3cad7df1f81eba5291
Towaiji <aliyasiraltowaiji@gmail.com>
2024-11-27 22:40:49 -0500

    implemented AdminApprovalOutputBoundary

--- src/main/Java/use_case/admin_account_approval/AdminApprovalOutputBoundary.java
+++ src/main/Java/use_case/admin_account_approval/AdminApprovalOutputBoundary.java
@@ -1,4 +1,6 @@
 package use_case.admin_account_approval;
 
 public interface AdminApprovalOutputBoundary {
+    void presentSuccess(AdminApprovalOutputData outputData);
+    void presentFailure(String errorMessage);
 }
