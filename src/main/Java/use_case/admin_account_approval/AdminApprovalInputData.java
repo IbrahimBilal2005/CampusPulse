a6d64e5afb88ca1f12bacd8477d2269a594ef9e2
Towaiji <aliyasiraltowaiji@gmail.com>
2024-11-27 22:40:23 -0500

    implemented AdminApprovalInputData

--- src/main/Java/use_case/admin_account_approval/AdminApprovalInputData.java
+++ src/main/Java/use_case/admin_account_approval/AdminApprovalInputData.java
@@ -1,4 +1,19 @@
 package use_case.admin_account_approval;
 
 public class AdminApprovalInputData {
+    private final String adminUid;
+    private final String targetUid;
+
+    public AdminApprovalInputData(String adminUid, String targetUid) {
+        this.adminUid = adminUid;
+        this.targetUid = targetUid;
+    }
+
+    public String getAdminUid() {
+        return adminUid;
+    }
+
+    public String getTargetUid() {
+        return targetUid;
+    }
 }
