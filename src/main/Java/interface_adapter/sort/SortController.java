10f453c71c12564d705bb2167c6fcdf43a03706c
IbrahimBilal2005 <ibrahim.bilal@mail.utoronto.ca>
2024-11-28 12:47:34 -0500

    completed sort controller and sort state

---
+++ src/main/Java/interface_adapter/sort/SortController.java
@@ -0,0 +1,21 @@
+package interface_adapter.sort;
+
+import entity.Event;
+import use_case.sort.SortInputBoundary;
+import use_case.sort.SortInputData;
+
+import java.util.List;
+
+public class SortController {
+
+    private final SortInputBoundary inputBoundary;
+
+    public SortController(SortInputBoundary inputBoundary) {
+        this.inputBoundary = inputBoundary;
+    }
+
+    public void sort(String query, List<Event> filteredEvents) {
+        SortInputData inputData = new SortInputData(query, filteredEvents);
+        inputBoundary.sort(inputData);
+    }
+}
