b4c8a6bf1274e836fcb8aacd4ef3f56e7e1f3b23
Towaiji <aliyasiraltowaiji@gmail.com>
2024-11-28 16:08:16 -0500

    fixed AdminApprovalUserDataAccessInterface

--- src/main/Java/use_case/admin_account_approval/AdminApprovalUserDataAccessInterface.java
+++ src/main/Java/use_case/admin_account_approval/AdminApprovalUserDataAccessInterface.java
@@ -1,6 +1,5 @@
 package use_case.admin_account_approval;
 
 public interface AdminApprovalUserDataAccessInterface {
-    boolean isAdmin(String uid);
     boolean approveUserAsEventPoster(String uid);
 }
