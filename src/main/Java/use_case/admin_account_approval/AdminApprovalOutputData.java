6bd7351fc6ad7f92f7382e63b563bdcd0541dc5e
Towaiji <aliyasiraltowaiji@gmail.com>
2024-11-28 16:08:25 -0500

    fixed AdminApprovalOutputData

--- src/main/Java/use_case/admin_account_approval/AdminApprovalOutputData.java
+++ src/main/Java/use_case/admin_account_approval/AdminApprovalOutputData.java
@@ -2,12 +2,18 @@ package use_case.admin_account_approval;
 
 public class AdminApprovalOutputData {
     private final String message;
+    private final boolean approved;
 
-    public AdminApprovalOutputData(String message) {
+    public AdminApprovalOutputData(String message, boolean approved) {
         this.message = message;
+        this.approved = approved;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public boolean getApproved() {
+        return approved;
     }
 }
