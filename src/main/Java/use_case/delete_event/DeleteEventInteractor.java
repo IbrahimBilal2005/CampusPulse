a9f08be46bb614aa0f4344666d6de8ef94462e00
IbrahimBilal2005 <ibrahim.bilal@mail.utoronto.ca>
2024-11-30 12:42:27 -0500

    Base implementation for DeleteEventInteractor.java

--- src/main/Java/use_case/delete_event/DeleteEventInteractor.java
+++ src/main/Java/use_case/delete_event/DeleteEventInteractor.java
@@ -1,4 +1,35 @@
 package use_case.delete_event;
 
-public class DeleteEventInteractor {
+import entity.AccountCreationStrategy;
+import entity.EventPoster;
+
+/**
+ * The Delete Event interactor.
+ */
+public class DeleteEventInteractor implements DeleteEventInputBoundary{
+    private final DeleteEventDataAccessInterface userDataAccessObject;
+    private final DeleteEventOutputBoundary userPresenter;
+    private final AccountCreationStrategy accountCreator;
+
+    public DeleteEventInteractor(DeleteEventDataAccessInterface deleteEventDataAccessInterface,
+                            DeleteEventOutputBoundary deleteEventOutputBoundary,
+                            AccountCreationStrategy accountCreator) {
+        this.userDataAccessObject = deleteEventDataAccessInterface;
+        this.userPresenter = deleteEventOutputBoundary;
+        this.accountCreator = accountCreator;
+
+    }
+
+    @Override
+    public void deleteEvent(DeleteEventInputData deleteEventInputData) {
+        // Get the eventPoster who's username mathces the input username
+        final EventPoster eventPoster = userDataAccessObject.getUser(deleteEventInputData.getUsername());
+
+        // Delete the event from the eventposter's events.
+        userDataAccessObject.deleteEvent(eventPoster, deleteEventInputData.getEventToDelete());
+
+        final DeleteEventOutputData deleteEventOutputData = new DeleteEventOutputData(eventPoster.getEvents(), false);
+
+        userPresenter.prepareSuccessView(deleteEventOutputData);
+    }
 }
