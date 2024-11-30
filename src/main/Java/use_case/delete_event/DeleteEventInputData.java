6f11f5d02e19bd5022ee4e853c86b3b7da400a9b
IbrahimBilal2005 <ibrahim.bilal@mail.utoronto.ca>
2024-11-30 12:30:34 -0500

    base implementation for DeleteEventInputBoundary.java and DeleteEventInputData.java

--- src/main/Java/use_case/delete_event/DeleteEventInputData.java
+++ src/main/Java/use_case/delete_event/DeleteEventInputData.java
@@ -1,4 +1,17 @@
 package use_case.delete_event;
 
-public interface DeleteEventInputData {
+import entity.Event;
+
+public class DeleteEventInputData {
+    private final String username;
+    private final Event eventToDelete;
+
+    public DeleteEventInputData(String username, Event eventToDelete) {
+        this.username = username;
+        this.eventToDelete = eventToDelete;
+    }
+
+    String getUsername() { return username; }
+
+    String getEventToDelete() { return eventToDelete.toString(); }
 }
