39e035412c0e24ac5fa5066ce24b7c07a17a8efd
IbrahimBilal2005 <ibrahim.bilal@mail.utoronto.ca>
2024-11-30 12:54:35 -0500

    output data and output boundary base implementation

--- src/main/Java/use_case/delete_event/DeleteEventOutputData.java
+++ src/main/Java/use_case/delete_event/DeleteEventOutputData.java
@@ -1,4 +1,20 @@
 package use_case.delete_event;
 
+import entity.Event;
+
+import java.util.List;
+
+/**
+ * Output Data for the Delete Event use case.
+ */
 public class DeleteEventOutputData {
+    private List<Event> events;
+    private boolean useCaseFailed;
+
+    public DeleteEventOutputData(List<Event> events) {
+        this.events = events;
+    }
+
+    public List<Event> getEvents() { return events; }
+    public boolean isUseCaseFailed() { return useCaseFailed; }
 }
