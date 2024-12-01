10fea0131a9806ff35f22120fd83615087e6671d
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-01 14:37:38 -0500

    added reject button logic to AdminApprovalUserDataAccessInterface

--- src/main/Java/use_case/admin_account_approval/AdminApprovalUserDataAccessInterface.java
+++ src/main/Java/use_case/admin_account_approval/AdminApprovalUserDataAccessInterface.java
@@ -2,4 +2,5 @@ package use_case.admin_account_approval;
 
 public interface AdminApprovalUserDataAccessInterface {
     boolean approveUserAsEventPoster(String uid);
+    boolean rejectUserAsEventPoster(String uid); // Renamed for consistency
 }
