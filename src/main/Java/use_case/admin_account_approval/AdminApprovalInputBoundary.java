ae0f83f0be7138186b299782422f907f70a795e1
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-01 14:37:22 -0500

    added reject button logic to AdminApprovalInputBoundary

--- src/main/Java/use_case/admin_account_approval/AdminApprovalInputBoundary.java
+++ src/main/Java/use_case/admin_account_approval/AdminApprovalInputBoundary.java
@@ -2,4 +2,5 @@ package use_case.admin_account_approval;
 
 public interface AdminApprovalInputBoundary {
     void approveUser(AdminApprovalInputData inputData);
+    void rejectUser(AdminApprovalInputData inputData); // Added reject method
 }
