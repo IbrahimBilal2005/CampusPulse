13aa94ab4f5c2926ab9f1f1370a9fc33c2683049
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-02 18:10:24 -0500

    implemented HomeScreenViewModel

--- src/main/Java/interface_adapter/home/HomeScreenViewModel.java
+++ src/main/Java/interface_adapter/home/HomeScreenViewModel.java
@@ -5,9 +5,11 @@ import entity.Event;
 import java.util.List;
 
 public class HomeScreenViewModel {
-    private boolean isEventPoster;
-    private List<Event> events;
+    private boolean isEventPoster; // Whether the user is an Event Poster
+    private List<Event> events; // Events to display on the Home Screen
+    private Event selectedEvent; // The event selected by the user for detailed view
 
+    // Getter and Setter for isEventPoster
     public boolean isEventPoster() {
         return isEventPoster;
     }
@@ -16,11 +18,26 @@ public class HomeScreenViewModel {
         isEventPoster = eventPoster;
     }
 
+    // Getter and Setter for events
     public List<Event> getEvents() {
         return events;
     }
 
     public void setEvents(List<Event> events) {
         this.events = events;
+    }
+
+    // Getter and Setter for selectedEvent
+    public Event getSelectedEvent() {
+        return selectedEvent;
+    }
+
+    public void setSelectedEvent(Event selectedEvent) {
+        this.selectedEvent = selectedEvent;
+    }
+
+    // Notify the UI to switch to the Event Details screen
+    public void switchToEventDetailsScreen() {
+        System.out.println("Switching to Event Details screen for event: " + selectedEvent.getName());
     }
 }
