832eb63683c5b79f41cedf3525e026f18ab9ea07
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-02 13:32:38 -0500

    implemented HomeScreenDataAccessInterface

--- src/main/Java/use_case/home/HomeScreenDataAccessInterface.java
+++ src/main/Java/use_case/home/HomeScreenDataAccessInterface.java
@@ -1,4 +1,10 @@
 package use_case.home;
 
+import entity.Event;
+import entity.User;
+
+import java.util.List;
+
 public interface HomeScreenDataAccessInterface {
+    List<Event> fetchEvents(User user);
 }
