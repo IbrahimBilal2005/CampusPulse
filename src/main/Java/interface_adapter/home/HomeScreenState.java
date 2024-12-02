6c48c6fbc974dc219fe5773863d48c837d230b4d
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-02 18:10:18 -0500

    implemented HomeScreenState

--- src/main/Java/interface_adapter/home/HomeScreenState.java
+++ src/main/Java/interface_adapter/home/HomeScreenState.java
@@ -1,4 +1,38 @@
 package interface_adapter.home;
 
+import entity.Event;
+
+import java.util.List;
+
 public class HomeScreenState {
+    private boolean isEventPoster; // Whether the user is an Event Poster
+    private List<Event> events; // List of events to display on the home screen
+    private Event selectedEvent; // The event currently being viewed in detail
+
+    // Getter and setter for isEventPoster
+    public boolean isEventPoster() {
+        return isEventPoster;
+    }
+
+    public void setEventPoster(boolean isEventPoster) {
+        this.isEventPoster = isEventPoster;
+    }
+
+    // Getter and setter for events
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
+    // Getter and setter for selectedEvent
+    public Event getSelectedEvent() {
+        return selectedEvent;
+    }
+
+    public void setSelectedEvent(Event selectedEvent) {
+        this.selectedEvent = selectedEvent;
+    }
 }
