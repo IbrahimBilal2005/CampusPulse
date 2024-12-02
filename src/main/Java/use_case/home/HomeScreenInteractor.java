f3231dd84279fe6b723d129aeec1aa8cf0c8acf9
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-02 13:32:55 -0500

    implemented HomeScreenInteractor

--- src/main/Java/use_case/home/HomeScreenInteractor.java
+++ src/main/Java/use_case/home/HomeScreenInteractor.java
@@ -1,4 +1,24 @@
 package use_case.home;
 
-public class HomeScreenInteractor {
+import entity.User;
+import use_case.home.HomeScreenDataAccessInterface;
+
+public class HomeScreenInteractor implements HomeScreenInputBoundary {
+    private final HomeScreenDataAccessInterface dataAccess;
+    private final HomeScreenOutputBoundary outputBoundary;
+
+    public HomeScreenInteractor(HomeScreenDataAccessInterface dataAccess, HomeScreenOutputBoundary outputBoundary) {
+        this.dataAccess = dataAccess;
+        this.outputBoundary = outputBoundary;
+    }
+
+    @Override
+    public void handleRequest(HomeScreenInputData inputData) {
+        User user = inputData.getUser();
+        boolean isEventPoster = user.isEventPoster();
+        var events = dataAccess.fetchEvents(user);
+
+        HomeScreenOutputData outputData = new HomeScreenOutputData(isEventPoster, events);
+        outputBoundary.presentHomeScreen(outputData);
+    }
 }
