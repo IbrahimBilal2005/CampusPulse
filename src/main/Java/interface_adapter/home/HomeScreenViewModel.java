7d4c1f0f65ac56b77b7b55e034dbb031f51b2f62
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-02 13:33:18 -0500

    implemented HomeScreenViewModel

--- src/main/Java/interface_adapter/home/HomeScreenViewModel.java
+++ src/main/Java/interface_adapter/home/HomeScreenViewModel.java
@@ -1,4 +1,26 @@
 package interface_adapter.home;
 
+import entity.Event;
+
+import java.util.List;
+
 public class HomeScreenViewModel {
+    private boolean isEventPoster;
+    private List<Event> events;
+
+    public boolean isEventPoster() {
+        return isEventPoster;
+    }
+
+    public void setEventPoster(boolean eventPoster) {
+        isEventPoster = eventPoster;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
 }
