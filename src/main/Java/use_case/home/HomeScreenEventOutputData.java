5d52498966550524220c0dab36c6832a7215852c
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-02 18:09:28 -0500

    implemented HomeScreenEventOutputData

--- src/main/Java/use_case/home/HomeScreenEventOutputData.java
+++ src/main/Java/use_case/home/HomeScreenEventOutputData.java
@@ -1,4 +1,15 @@
 package use_case.home;
 
+import entity.Event;
+
 public class HomeScreenEventOutputData {
+    private final Event event;
+
+    public HomeScreenEventOutputData(Event event) {
+        this.event = event;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
 }
