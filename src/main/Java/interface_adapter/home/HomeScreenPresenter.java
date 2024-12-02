db30f528ee464c1eb3f372827e979cdcfcd702d6
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-02 13:33:13 -0500

    implemented HomeScreenPresenter

--- src/main/Java/interface_adapter/home/HomeScreenPresenter.java
+++ src/main/Java/interface_adapter/home/HomeScreenPresenter.java
@@ -1,4 +1,18 @@
 package interface_adapter.home;
 
-public class HomeScreenPresenter {
+import use_case.home.HomeScreenOutputBoundary;
+import use_case.home.HomeScreenOutputData;
+
+public class HomeScreenPresenter implements HomeScreenOutputBoundary {
+    private final HomeScreenViewModel viewModel;
+
+    public HomeScreenPresenter(HomeScreenViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    @Override
+    public void presentHomeScreen(HomeScreenOutputData outputData) {
+        viewModel.setEventPoster(outputData.isEventPoster());
+        viewModel.setEvents(outputData.getEvents());
+    }
 }
