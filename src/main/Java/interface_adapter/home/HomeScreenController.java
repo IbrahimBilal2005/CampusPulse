bc97f1cbcd5feedf5d3c77c0323e9eeb898e6d3f
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-02 18:09:04 -0500

    implemented HomeScreenController

--- src/main/Java/interface_adapter/home/HomeScreenController.java
+++ src/main/Java/interface_adapter/home/HomeScreenController.java
@@ -2,8 +2,10 @@ package interface_adapter.home;
 
 import use_case.home.HomeScreenInputBoundary;
 import use_case.home.HomeScreenInputData;
+import use_case.home.HomeScreenEventInputData;
 
 import entity.User;
+import entity.Event;
 
 public class HomeScreenController {
     private final HomeScreenInputBoundary inputBoundary;
@@ -12,8 +14,23 @@ public class HomeScreenController {
         this.inputBoundary = inputBoundary;
     }
 
-    public void handleRequest(User user) {
+    /**
+     * Handles fetching the user's events to display on the home screen.
+     *
+     * @param user The currently logged-in user.
+     */
+    public void fetchMyEvents(User user) {
         HomeScreenInputData inputData = new HomeScreenInputData(user);
-        inputBoundary.handleRequest(inputData);
+        inputBoundary.loadMyEvents(inputData); // Triggers interactor logic for "My Events"
+    }
+
+    /**
+     * Handles fetching details for a specific event when clicked.
+     *
+     * @param event The event that the user clicked on.
+     */
+    public void fetchEventDetails(Event event) {
+        HomeScreenEventInputData inputData = new HomeScreenEventInputData(event);
+        inputBoundary.loadEventDetails(inputData); // Triggers interactor logic for event details
     }
 }
