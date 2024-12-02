17867c34706a5582176f8743140ea2ab6831c19e
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-02 13:32:31 -0500

    implemented HomeScreenController

--- src/main/Java/interface_adapter/home/HomeScreenController.java
+++ src/main/Java/interface_adapter/home/HomeScreenController.java
@@ -1,4 +1,19 @@
 package interface_adapter.home;
 
+import use_case.home.HomeScreenInputBoundary;
+import use_case.home.HomeScreenInputData;
+
+import entity.User;
+
 public class HomeScreenController {
+    private final HomeScreenInputBoundary inputBoundary;
+
+    public HomeScreenController(HomeScreenInputBoundary inputBoundary) {
+        this.inputBoundary = inputBoundary;
+    }
+
+    public void handleRequest(User user) {
+        HomeScreenInputData inputData = new HomeScreenInputData(user);
+        inputBoundary.handleRequest(inputData);
+    }
 }
