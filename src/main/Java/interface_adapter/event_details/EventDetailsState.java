f46bea32bcabfd28b6ddaefc539fa2dcc4f04739
Tanay Langhe <tanaylanghe@gmail.com>
2024-12-03 08:33:04 -0500

    Added documentation for event details state

--- src/main/Java/interface_adapter/event_details/EventDetailsState.java
+++ src/main/Java/interface_adapter/event_details/EventDetailsState.java
@@ -1,4 +1,8 @@
 package interface_adapter.event_details;
 
+/**
+ * The state for the event details model
+ * This class is empty because there is no information within the view that needs to be saved
+ */
 public class EventDetailsState {
 }
