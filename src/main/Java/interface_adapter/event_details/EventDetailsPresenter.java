e415a38cdfc3046d43db97c3701728169bd7adde
Tanay Langhe <tanaylanghe@gmail.com>
2024-12-03 01:18:07 -0500

    Created correct constructor to allow for view swap

--- src/main/Java/interface_adapter/event_details/EventDetailsPresenter.java
+++ src/main/Java/interface_adapter/event_details/EventDetailsPresenter.java
@@ -1,19 +1,26 @@
 package interface_adapter.event_details;
 
+import interface_adapter.ViewManagerModel;
+import interface_adapter.home.HomeScreenState;
+import interface_adapter.home.HomeScreenViewModel;
 import use_case.event_details.EventDetailsOutputBoundary;
 import use_case.event_details.EventDetailsOutputData;
 
 public class EventDetailsPresenter implements EventDetailsOutputBoundary {
-    private final EventDetailsViewModel viewModel;
+    private final EventDetailsViewModel eventViewModel;
+    private final ViewManagerModel viewManagerModel;
+    private final HomeScreenViewModel homeScreenViewModel;
 
-    public EventDetailsPresenter(EventDetailsViewModel viewModel){
-        this.viewModel = viewModel;
+    public EventDetailsPresenter(EventDetailsViewModel viewModel, EventDetailsViewModel eventViewModel, ViewManagerModel viewManagerModel, HomeScreenViewModel homeScreenViewModel){
+        this.eventViewModel = eventViewModel;
+        this.viewManagerModel = viewManagerModel;
+        this.homeScreenViewModel = homeScreenViewModel;
     }
 
 
     public void prepareSuccessView(EventDetailsOutputData data){
         if (!data.isClick()) {
-//      TODO viewManagerModel.setState(homeViewModel.getViewName());
+
         }
     }
 }
