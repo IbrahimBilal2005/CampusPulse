2a9680b01796fd4d3984cc3c7bac66a3643f3c94
AhmadIbrahimWaqas <157913756+AhmadIbrahimWaqas@users.noreply.github.com>
2024-12-03 18:58:01 -0500

    Adding javadocs

--- src/main/Java/use_case/change_password/ChangePasswordOutputBoundary.java
+++ src/main/Java/use_case/change_password/ChangePasswordOutputBoundary.java
@@ -1,6 +1,20 @@
 package use_case.change_password;
 
+/**
+ * The output boundary for the Change Password Use Case.
+ */
+
 public interface ChangePasswordOutputBoundary {
+
+    /**
+     * The function showing success for the Change Password Use Case.
+     */
+
     void presentSuccess(ChangePasswordOutputData outputData);
+
+    /**
+     * The function showing failure for the Change Password Use Case.
+     */
+
     void presentFailure(String errorMessage);
 }
