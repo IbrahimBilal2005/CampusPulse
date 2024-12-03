903cfb927083e79fe7c36a10d058ea39101bb8f7
Tanay Langhe <tanaylanghe@gmail.com>
2024-12-03 08:50:19 -0500

    Accidently removed constants that were needed, reintroducing them now

--- src/main/Java/interface_adapter/event_details/EventDetailsViewModel.java
+++ src/main/Java/interface_adapter/event_details/EventDetailsViewModel.java
@@ -7,6 +7,12 @@ import interface_adapter.ViewModel;
  */
 public class EventDetailsViewModel extends ViewModel<EventDetailsState> {
 
+    public final String DATE = "Date: ";
+    public final String LOCATION = "Location: ";
+    public final String TIME = "Time: ";
+    public final String TAGS = "Tags: ";
+    public final String BACK = "Back";
+
     public EventDetailsViewModel() {
         super("EventDetails_screen");
         setState(new EventDetailsState());
