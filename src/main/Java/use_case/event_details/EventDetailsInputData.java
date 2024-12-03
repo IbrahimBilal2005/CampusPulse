c889326f5ff97019d98eaed9b0a738bee617b271
Tanay Langhe <tanaylanghe@gmail.com>
2024-12-03 08:12:55 -0500

    Added documentation for the Input Data class

--- src/main/Java/use_case/event_details/EventDetailsInputData.java
+++ src/main/Java/use_case/event_details/EventDetailsInputData.java
@@ -1,4 +1,8 @@
 package use_case.event_details;
 
+/**
+ * The Input Data for the event details use case
+ * However since there is no required input data it is blank
+ */
 public class EventDetailsInputData {
 }
