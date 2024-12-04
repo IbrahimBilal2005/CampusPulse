466943d37caf66911f165a25c7eda84fe77dff54
Towaiji <aliyasiraltowaiji@gmail.com>
2024-12-03 20:07:51 -0500

    save changes

--- src/main/Java/use_case/admin_account_approval/AdminApprovalUserDataAccessInterface.java
+++ src/main/Java/use_case/admin_account_approval/AdminApprovalUserDataAccessInterface.java
@@ -1,6 +1,11 @@
 package use_case.admin_account_approval;
 
+import entity.EventPoster;
+
+import java.util.List;
+
 public interface AdminApprovalUserDataAccessInterface {
-    boolean approveUserAsEventPoster(String uid);
+    boolean setApproval(String uid, boolean approvalState);
     boolean rejectUserAsEventPoster(String uid);
+    List<EventPoster> getUnapprovedUsers();
 }
